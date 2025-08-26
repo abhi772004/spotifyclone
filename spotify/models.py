22b95be411751d8b080e20a135fdd49501c257d9
abhi772004 <abhi98158@gmail.com>
2025-08-26 14:57:51 +0530

    added drinda maan and shaktipooja bio

--- spotify/models.py
+++ spotify/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Artist(models.Model):
     name=models.CharField(max_length=100,null=True, blank=True)
     image=models.ImageField(upload_to='artists/',null=True, blank=True)
-
+    desc=models.TextField(null=True, blank=True)
     def __str__(self):
      return self.name
 
